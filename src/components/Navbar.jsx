fc18668ea3a6e062becf582627ad4bcfeab0d6f0
vidhanshu <vidhanshu7218555039@gmail.com>
2022-02-13 18:27:28 +0530

    on scroll navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,39 @@ import { Link } from "react-router-dom";
 import Button from "./Button";
 function Navbar() {
   const [click, setClick] = useState(false);
-
+  const [checkScrollDown, setCheckScrollDown] = useState(false);
   const handleClick = () => {
     setClick((pre) => !pre);
   };
   const closeMobileMenu = () => {
     setClick(false);
   };
+  useEffect(() => {
+    var lastScrollTop = 0;
 
+    // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+    window.addEventListener(
+      "scroll",
+      function () {
+        // or window.addEventListener("scroll"....
+        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+        if (st > lastScrollTop) {
+          setCheckScrollDown(true);
+        } else {
+          setCheckScrollDown(false);
+        }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+      },
+      false
+    );
+  }, []);
   return (
     <nav className={style.navbar}>
-      <div className={style.navbar_container}>
+      <div
+        className={`${style.navbar_container} ${
+          checkScrollDown ? style.make_opacity_zero : style.make_opacity_one
+        }`}
+      >
         <Link to="/">
           <h1 className={style.logo}>
             VTRL <i className="fab fa-accusoft"></i>
