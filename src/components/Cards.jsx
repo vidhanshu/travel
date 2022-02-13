9995397a6dbdedf1e532bafad7c47810a06a81ec
vidhanshu <vidhanshu7218555039@gmail.com>
2022-02-13 18:41:35 +0530

    heading fro travel comp

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,19 +4,33 @@ import { card_details } from "../api/card-details";
 import CardItem from "./CardItem";
 function Cards() {
   return (
-    <div className={style.cards_container}>
-      {card_details.map((e, idx) => {
-        return (
-          <CardItem
-            key={idx}
-            to={e.to}
-            heading={e.heading}
-            img={e.img}
-            label={e.label}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div style={{ width: "100%" }}>
+        <h1
+          style={{
+            margin: "30px 0px",
+            fontSize: "40px",
+            textAlign: "center",
+            padding: "5px",
+          }}
+        >
+          Explore the world with us
+        </h1>
+        <div className={style.cards_container}>
+          {card_details.map((e, idx) => {
+            return (
+              <CardItem
+                key={idx}
+                to={e.to}
+                heading={e.heading}
+                img={e.img}
+                label={e.label}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
